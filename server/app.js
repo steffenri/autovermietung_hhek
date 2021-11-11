d14af059e4fccfe8c0779b6c8acf134513e71eb7
Steffen <ring@kitconcept.com>
2021-11-11 12:29:18 +0100

    macht absolut alles keinen sinn hier

--- server/app.js
+++ server/app.js
@@ -1,18 +1,37 @@
 var restify = require("restify");
 var errors = require("restify-errors");
 const port = process.env.PORT || 8080;
+let endpoint = "/api";
 
 function respond(req, res, next) {
   res.send("hello " + req.params.name);
   next();
 }
 
-function post(request, respond, next) {
-  body = request.params.body;
-  respond.send(200);
-  console.log(body);
-  next();
-}
+// function post(req, res, next) {
+//   let body = req;
+//   res.send(200);
+//   console.log(
+//     "this is the respond:",
+//     res,
+//     "this is the request:",
+//     req,
+//     "this is next:",
+//     next
+//   );
+//   next();
+// }
+
+server.post("api/", function (req, res, next) {
+  if (req.body.message) {
+    var text = req.body.message;
+    console.log(text);
+    res.send(200, { response: "OK" });
+  } else {
+    res.send(400, { response: "Incorrect JSON structure" });
+  }
+  return next();
+});
 
 var server = restify.createServer({ name: "autovermietung_hhek" });
 server.get("/hello/:name", respond);
--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
+  const apiPath = "http://localhost:8080";
+  const [body, setBody] = useState("");
+  const [result, setResult] = useState({ body: "", data: {} });
+  console.log(result && result);
+
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
+
+  const postReq = () => {
+    fetch(`http://localhost:8080/api/test`, requestOptions)
+      .then((response) => {
+        console.log("this is the response:", response);
+        setResult({ body: response.json() }, ...result.data);
+      })
+      .then((data) => {
+        console.log("this is data:", data);
+        setResult(...result.body, { data: data.id });
+      });
+  };
 
   return (
     <div className="App">
@@ -25,6 +47,14 @@ function App() {
             >
               get request testen
             </a>
+            <br />
+            <input
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              placeholder="Text"
+            ></input>
+            <br />
+            <button onClick={() => postReq()}>Send Post</button>
           </div>
         </body>
       </header>
